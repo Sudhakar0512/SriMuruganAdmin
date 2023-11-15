52298eeec68d308cf45cdfe1cc61c02244ad8ea2
Sudhakar0512 <sudhakar.s2021csbs@sece.ac.in>
2023-11-15 23:16:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminEdits from './Components/AdminEdits'
+import Gallery from './Components/Gallery'
+
+export default function App() {
+  return (
+    <div>
+      <AdminEdits/>
+      <Gallery/>
+    </div>
+  )
+}
---
+++ src/Components/AdminEdits.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../Components/AdminEdits.css";
+
+export default function AdminEdits() {
+  const [product, setProduct] = useState({
+    name: "",
+    imageFile: null, // for file input
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setProduct({ ...product, [name]: value });
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setProduct({ ...product, imageFile: file });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", product.name);
+    // formData.append('price', product.price);
+    // formData.append('quantity', product.quantity);
+    formData.append("imageFile", product.imageFile);
+
+    try {
+      await axios.post("http://localhost:8080/api/products/insert", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      // Reset the form or display a success message
+      console.log("Product added successfully");
+    } catch (error) {
+      console.error("Error adding product", error);
+    }
+  };
+
+  return (
+    <div className="div">
+      <h2 className="h2">Add Construction</h2>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="div">
+          <label className="labe;">Name:</label>
+          <input
+            className="input"
+            type="text"
+            name="name"
+            value={product.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="div">
+          <label className="label">Image:</label>
+          <input
+            className="input"
+            type="file"
+            name="imageFile"
+            onChange={handleFileChange}
+          />
+        </div>
+        <button type="submit" className="button">
+        Add Construction
+        </button>
+      </form>
+    </div>
+  );
+}
